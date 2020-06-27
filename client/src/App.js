15b5636a37387518294ce084ddaa806f3c7f72e0
Alex Boelens <a.boelens@live.nl>
2020-06-28 00:05:18 +0200

    user reducer and user actions to app and header added

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import { connect } from 'react-redux';
+import { setCurrentUser } from './redux/user/user-actions'
 import Header from './components/header/Header'
 import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shop/ShopPage';
 import SingInAndSignUpPage from './pages/sign-in-and-sign-up/Sign-in-and-sign-up';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
-const App = () => {
-  const [currentUser, setCurrentUser] = useState(null);
+const App = ({ setCurrentUser }) => {
 
   useEffect(() => {
     const unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
@@ -30,11 +31,11 @@ const App = () => {
       unsubscribeFromAuth()
     }
     // es-lint-disable-next-line
-  }, [])
+  }, [setCurrentUser])
 
   return (
     <div>
-      <Header currentUser={currentUser} />
+      <Header />
 
       <Switch>
         <Route exact path='/' component={HomePage} />
@@ -46,4 +47,8 @@ const App = () => {
   );
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
--- client/src/components/custom-button/Custom-button.jsx
+++ client/src/components/custom-button/Custom-button.jsx
@@ -4,7 +4,9 @@ import './Custom-button.styles.scss';
 
 const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => {
     return (
-        <button className={`${isGoogleSignIn && 'google-sign-in'} custom-button`} {...otherProps}>
+        <button
+            type="button"
+            className={`${isGoogleSignIn && 'google-sign-in'} custom-button`} {...otherProps}>
             {children}
         </button>
     )
