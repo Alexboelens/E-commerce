877dd2e7383f6c45058abfc9e590c54849194297
Alex Boelens <a.boelens@live.nl>
2020-07-03 21:47:39 +0200

    code updated

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from 'redux-persist/es/integration/react';
 
 
 ReactDOM.render(
